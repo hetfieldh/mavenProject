1f80590e54fb7f13cd5b0dc3ea6b808f89be40d0
Douglas Osorio <douglas.anderson7@gmail.com>
2021-10-16 20:01:40 -0300

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/hetfieldh/mavenproject/domain/Categoria.java
+++ src/main/java/com/hetfieldh/mavenproject/domain/Categoria.java
@@ -11,6 +11,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
 @Entity
 public class Categoria implements Serializable {
 
@@ -22,6 +24,7 @@ public class Categoria implements Serializable {
 	private String nome;
 
 	// associacao de 1 para * (muitos)
+	@JsonManagedReference
 	@ManyToMany(mappedBy = "categorias")
 	private List<Produto> produtos = new ArrayList<>();
 
--- src/main/java/com/hetfieldh/mavenproject/services/CategoriaService.java
+++ src/main/java/com/hetfieldh/mavenproject/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.hetfieldh.mavenproject.domain.Categoria;
 import com.hetfieldh.mavenproject.repositories.CategoriaRepository;
+import com.hetfieldh.mavenproject.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
 
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria find(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-
 }
