2242f0c2ea17d265c5d36dddeac89a9cc8cd0bdd
Douglas Osorio <douglas.anderson7@gmail.com>
2021-10-17 01:01:47 -0300

    Estado e Cidade

--- src/main/java/com/hetfieldh/mavenproject/MavenProjectApplication.java
+++ src/main/java/com/hetfieldh/mavenproject/MavenProjectApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.hetfieldh.mavenproject.domain.Categoria;
+import com.hetfieldh.mavenproject.domain.Cidade;
+import com.hetfieldh.mavenproject.domain.Estado;
 import com.hetfieldh.mavenproject.domain.Produto;
 import com.hetfieldh.mavenproject.repositories.CategoriaRepository;
+import com.hetfieldh.mavenproject.repositories.CidadeRepository;
+import com.hetfieldh.mavenproject.repositories.EstadoRepository;
 import com.hetfieldh.mavenproject.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,6 +23,10 @@ public class MavenProjectApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
+	@Autowired
+	private CidadeRepository cidadeRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenProjectApplication.class, args);
@@ -26,20 +34,34 @@ public class MavenProjectApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		// cria as categorias
+		// instancia as categorias
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 
-		// cria os produtos
+		// instancia os produtos
 		Produto p1 = new Produto(null, "Computador", 2000.00);
 		Produto p2 = new Produto(null, "Impressora", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
+
+		// instancia os estados
+		Estado est1 = new Estado(null, "Minas Gerais");
+		Estado est2 = new Estado(null, "Sao Paulo");
+
+		// instancia as cidades
+		Cidade c1 = new Cidade(null, "Uberlandia", est1); // aqui ja fez a atribuicao do estado nas cidades atraves do
+															// construtor
+		Cidade c2 = new Cidade(null, "Sao Paulo", est2);
+		Cidade c3 = new Cidade(null, "Campinas", est2);
+
+		// accocia as cidades aos estados
+		est1.getCidades().addAll(Arrays.asList(c1));
+		est2.getCidades().addAll(Arrays.asList(c2, c3));
 
 		// associa todos os produtos as categorias
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
-		// asscoai todas as categorias aos produtos
+		// associa todas as categorias aos produtos
 		p1.getCategorias().addAll(Arrays.asList(cat1));
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
@@ -49,5 +71,12 @@ public class MavenProjectApplication implements CommandLineRunner {
 
 		// salva os produtos no BD
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
+
+		// salva os Estados no BD
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+
+		// salva as Cidades no BD
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
+
 	}
 }
