521a2d840c8dc0f44d6daa39b8ccd4d439a6c767
Douglas Osorio <douglas.anderson7@gmail.com>
2021-10-20 19:26:47 -0300

    ItemPedido e ItemPedidoPK

--- src/main/java/com/hetfieldh/mavenproject/MavenProjectApplication.java
+++ src/main/java/com/hetfieldh/mavenproject/MavenProjectApplication.java
@@ -13,6 +13,7 @@ import com.hetfieldh.mavenproject.domain.Cidade;
 import com.hetfieldh.mavenproject.domain.Cliente;
 import com.hetfieldh.mavenproject.domain.Endereco;
 import com.hetfieldh.mavenproject.domain.Estado;
+import com.hetfieldh.mavenproject.domain.ItemPedido;
 import com.hetfieldh.mavenproject.domain.Pagamento;
 import com.hetfieldh.mavenproject.domain.PagamentoComBoleto;
 import com.hetfieldh.mavenproject.domain.PagamentoComCartao;
@@ -25,6 +26,7 @@ import com.hetfieldh.mavenproject.repositories.CidadeRepository;
 import com.hetfieldh.mavenproject.repositories.ClienteRepository;
 import com.hetfieldh.mavenproject.repositories.EnderecoRepository;
 import com.hetfieldh.mavenproject.repositories.EstadoRepository;
+import com.hetfieldh.mavenproject.repositories.ItemPedidoRepository;
 import com.hetfieldh.mavenproject.repositories.PagamentoRepository;
 import com.hetfieldh.mavenproject.repositories.PedidoRepository;
 import com.hetfieldh.mavenproject.repositories.ProdutoRepository;
@@ -48,6 +50,8 @@ public class MavenProjectApplication implements CommandLineRunner {
 	private PedidoRepository pedidoRepository;
 	@Autowired
 	private PagamentoRepository pagamentoRepository;
+	@Autowired
+	private ItemPedidoRepository itemPedidoRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(MavenProjectApplication.class, args);
@@ -121,12 +125,26 @@ public class MavenProjectApplication implements CommandLineRunner {
 		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
 		ped1.setPagamento(pagto1);
 
-		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"), null);
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2, sdf.parse("20/10/2017 00:00"),
+				null);
 		ped2.setPagamento(pagto2);
 
 		cli1.getPedidos().addAll(Arrays.asList(ped1, ped2));
 
 		pedidoRepository.saveAll(Arrays.asList(ped1, ped2));
 		pagamentoRepository.saveAll(Arrays.asList(pagto1, pagto2));
+
+		ItemPedido ip1 = new ItemPedido(ped1, p1, 0.00, 1, 2000.00);
+		ItemPedido ip2 = new ItemPedido(ped1, p3, 0.00, 2, 80.00);
+		ItemPedido ip3 = new ItemPedido(ped2, p2, 100.00, 1, 800.00);
+
+		ped1.getItens().addAll(Arrays.asList(ip1, ip2));
+		ped2.getItens().addAll(Arrays.asList(ip3));
+
+		p1.getItens().addAll(Arrays.asList(ip1));
+		p2.getItens().addAll(Arrays.asList(ip3));
+		p3.getItens().addAll(Arrays.asList(ip2));
+
+		itemPedidoRepository.saveAll(Arrays.asList(ip1, ip2, ip3));
 	}
 }
--- src/main/java/com/hetfieldh/mavenproject/domain/Pedido.java
+++ src/main/java/com/hetfieldh/mavenproject/domain/Pedido.java
@@ -2,7 +2,9 @@ package com.hetfieldh.mavenproject.domain;
 
 import java.io.Serializable;
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -11,6 +13,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
 
 @Entity
@@ -26,14 +29,17 @@ public class Pedido implements Serializable {
 	// associacao 1 para 1
 	@OneToOne(cascade = CascadeType.ALL, mappedBy = "pedido")
 	private Pagamento pagamento;
-	
+
 	@ManyToOne
-	@JoinColumn(name="cliente_id")
+	@JoinColumn(name = "cliente_id")
 	private Cliente cliente;
-	
+
 	@ManyToOne
-	@JoinColumn(name="endereco_de_entrega_id")
+	@JoinColumn(name = "endereco_de_entrega_id")
 	private Endereco enderecoDeEntrega;
+
+	@OneToMany(mappedBy = "id.pedido")
+	private Set<ItemPedido> itens = new HashSet<>();
 
 	public Pedido() {
 	}
@@ -84,6 +90,14 @@ public class Pedido implements Serializable {
 
 	public void setEnderecoDeEntrega(Endereco enderecoDeEntrega) {
 		this.enderecoDeEntrega = enderecoDeEntrega;
+	}
+
+	public Set<ItemPedido> getItens() {
+		return itens;
+	}
+
+	public void setItens(Set<ItemPedido> itens) {
+		this.itens = itens;
 	}
 
 	@Override
