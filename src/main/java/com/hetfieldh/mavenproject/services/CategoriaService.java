ca7bd8927ca20b6a379717e9522196561c385cf4
Douglas Osorio <douglas.anderson7@gmail.com>
2021-10-24 14:30:17 -0300

    Deletando uma Categoria com DELETE

--- src/main/java/com/hetfieldh/mavenproject/services/CategoriaService.java
+++ src/main/java/com/hetfieldh/mavenproject/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.hetfieldh.mavenproject.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.hetfieldh.mavenproject.domain.Categoria;
 import com.hetfieldh.mavenproject.repositories.CategoriaRepository;
+import com.hetfieldh.mavenproject.services.exceptions.DataIntegrityException;
 import com.hetfieldh.mavenproject.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -20,14 +22,23 @@ public class CategoriaService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+
 	public Categoria insert(Categoria obj) {
 		obj.setId(null);
 		return repo.save(obj);
 	}
-	
+
 	public Categoria update(Categoria obj) {
 		find(obj.getId());
 		return repo.save(obj);
+	}
+
+	public void delete(Integer id) {
+		find(id);
+		try {
+			repo.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível excluir uma categoria que contém produtos cadastrados.");
+		}
 	}
 }
